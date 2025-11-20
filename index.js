bed2fecaf4d10ed32d07143b39056f448a54dc3d
ahsan.habib01 <ahshapon00@gmail.com>
2025-11-21 00:44:18 +0600

    get api

--- index.js
+++ index.js
@@ -28,7 +28,15 @@ async function run() {
 
     // parcel api
     app.get('/parcels', async (req, res) => {
-      
+      const query = {};
+      const { email } = req.query;
+      // /parcels?email=''&
+      if (email) {
+        query.senderEmail = email;
+      }
+      const cursor = parcelsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
     });
 
     app.post('/parcels', async (req, res) => {
